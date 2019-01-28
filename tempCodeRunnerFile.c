d3cdd203eda3278f693050c7ac5d54692e9862eb
= <=>
2019-01-28 16:59:32 +0330

    Debug Log

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,5 @@
-
-        for(j=0;j<8;j++){
-            printf("%2c",mainBoard[i][j]);
- 
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for( i=0;i<8;i++){
-        for(j=0;j<8;j++){
-            printf("%d ",secondBoard[i][j]);
- 
-        }
-        printf("\n");
-    }
+for (i = 0; i < 8; i++)
+    // {
+    //     for (j = 0; j < 8; j++)
+    //         mainBoard[i][j] = argv[i + 1][j];
+    
