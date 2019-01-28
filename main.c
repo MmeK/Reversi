c3aa97cf2988e7e3f82c5add10e6acb6173b9063
= <=>
2019-01-28 17:37:47 +0330

    I

--- main.c
+++ main.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-FILE *cfptr;
+// FILE *cfptr;
 void choosemax(int board1[8][8], int board2[8][8]);
 void CheckBoard(char board[8][8], char player, int secondBoard[8][8], int thirdBoard[8][8]);
-int CheckTile(char board[8][8], char player, int direction, int x, int y, int secondboard[8][8], int value);
+int CheckTile(char board[8][8], char player, int direction, int x, int y, int secondboard[8][8], int value, int optionalVal);
 int main(int argc, char *argv[])
 {
 
     char player;
     int i, j;
-    cfptr = fopen("PlayLog.txt", "r+");
-     char mainBoard[8][8]={{"00000000"},{"00000000"},{"00000000"},{"00021110"},{"00112000"},{"00000000"},{"00000000"},{"00000000"}};
+    // cfptr = fopen("PlayLog.txt", "r+");
+    //  char mainBoard[8][8]={{"00000000"},{"00000000"},{"00000000"},{"00021110"},{"00112000"},{"00000000"},{"00000000"},{"00000000"}};
     int secondBoard[8][8] = {{0}};
     const int thirdBoard[8][8] = {{100, -50, 20, 10, 10, 20, -50, 100}, {-50, -60, 0, 0, 0, 0, -60, -50}, {20, 0, 0, 0, 0, 0, 0, 20}, {10, 0, 0, 0, 0, 0, 0, 10}, {10, 0, 0, 0, 0, 0, 0, 10}, {20, 0, 0, 0, 0, 0, 0, 20}, {-50, -60, 0, 0, 0, 0, -60, -50}, {100, -50, 20, 10, 10, 20, -50, 100}};
     char mainBoard[8][8];
-    // for (i = 0; i < 8; i++)
-    // {
-    //     for (j = 0; j < 8; j++)
-    //         mainBoard[i][j] = argv[i + 1][j];
-    // }
-   // player = argv[i + 1][0];
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+            mainBoard[i][j] = argv[i + 1][j];
+    }
+    player = argv[i + 1][0];
 
-    
-     player='1';
+    //  player='1';
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
@@ -31,23 +30,25 @@ int main(int argc, char *argv[])
         }
     }
     CheckBoard(mainBoard, player, secondBoard, thirdBoard);
-    for( i=0;i<8;i++){
-        for(j=0;j<8;j++){
-            fprintf(cfptr,"%2c",mainBoard[i][j]);
-
-        }
-        fprintf(cfptr,"\n");
-    }
-    printf(cfptr,"\n");
-    for( i=0;i<8;i++){
-        for(j=0;j<8;j++){
-            fprintf(cfptr,"%d ",secondBoard[i][j]);
-
-        }
-        fprintf(cfptr,"\n");
-    }
+    // for (i = 0; i < 8; i++)
+    // {
+    //     for (j = 0; j < 8; j++)
+    //     {
+    //         fprintf(cfptr, "%2c", mainBoard[i][j]);
+    //     }
+    //     fprintf(cfptr, "\n");
+    // }
+    // fprintf(cfptr, "\n");
+    // for (i = 0; i < 8; i++)
+    // {
+    //     for (j = 0; j < 8; j++)
+    //     {
+    //         fprintf(cfptr, "%d ", secondBoard[i][j]);
+    //     }
+    //     fprintf(cfptr, "\n");
+    // }
     choosemax(secondBoard, thirdBoard);
-    fclose(cfptr);
+    // fclose(cfptr);
     return 0;
 }
 void CheckBoard(char board[8][8], char player, int secondBoard[8][8], int thirdBoard[8][8])
@@ -63,13 +64,13 @@ void CheckBoard(char board[8][8], char player, int secondBoard[8][8], int thirdB
                 for (direction = 1; direction < 9; direction++)
                 {
 
-                    CheckTile(board, player, direction, i, j, secondBoard, thirdBoard[i][j]);
+                    CheckTile(board, player, direction, i, j, secondBoard, 0,thirdBoard[i][j]);
                 }
             }
         }
     }
 }
-int CheckTile(char board[8][8], char player, int direction, int x, int y, int secondboard[8][8], int value)
+int CheckTile(char board[8][8], char player, int direction, int x, int y, int secondboard[8][8], int value, int optionalVal)
 {
 #pragma region SWITCH
     switch (direction)
@@ -77,42 +78,97 @@ int CheckTile(char board[8][8], char player, int direction, int x, int y, int se
     case (1):
         if (x < 7)
             x++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (2):
         if (x < 7)
             x++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         if (y > 0)
             y--;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (3):
         if (y > 0)
             y--;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (4):
         if (x > 0)
             x--;
         if (x > 0)
             y--;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (5):
         if (x > 0)
             x--;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (6):
         if (x > 0)
             x--;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         if (y < 7)
             y++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (7):
         if (y < 7)
             y++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     case (8):
         if (y < 7)
             y++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         if (x < 7)
             x++;
+        else
+        {
+            value = 0;
+            return 0;
+        }
         break;
     }
 #pragma endregion
@@ -125,7 +181,7 @@ int CheckTile(char board[8][8], char player, int direction, int x, int y, int se
             // printf("x=%d , y=%d\n",x,y);
             if (x < 0 || x > 7 || y < 0 || y > 7)
                 return 0;
-            CheckTile(board, player, direction, x, y, secondboard, value);
+            CheckTile(board, player, direction, x, y, secondboard, value,optionalVal);
         }
     }
     if (player == '2')
@@ -138,36 +194,25 @@ int CheckTile(char board[8][8], char player, int direction, int x, int y, int se
             // printf("x=%d , y=%d\n",x,y);
             if (x < 1 || x > 6 || y < 1 || y > 6)
                 return 0;
-            CheckTile(board, player, direction, x, y, secondboard, value);
+            CheckTile(board, player, direction, x, y, secondboard, value,optionalVal);
         }
     }
     if (board[x][y] == '0')
         if (value != 0)
-            secondboard[x][y] = value;
+            secondboard[x][y] = value+optionalVal;
     return 0;
 }
 
 void choosemax(int board1[8][8], int board2[8][8])
 {
-    int x=0, j, y=0, i, max;
+    int x = 0, j, y = 0, i, max;
     int board3[8][8];
 
     for (i = 0; i < 8; i++)
     {
         for (j = 0; j < 8; j++)
         {
-            board3[i][j] = board1[i][j] + board2[i][j];
-        }
-    }
-
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            if (board1[i][j] == 0)
-            {
-                board3[i][j] = 0;
-            }
+            board3[i][j] = (board1[i][j] == 0) ? 0 : board1[i][j] + board2[i][j];
         }
     }
 
@@ -176,7 +221,7 @@ void choosemax(int board1[8][8], int board2[8][8])
     {
         for (j = 0; j < 8; j++)
         {
-            if (board3[i][j]!=0&&board3[i][j] > max)
+            if (board3[i][j] != 0 && board3[i][j] > max)
             {
 
                 max = board3[i][j];
@@ -185,5 +230,6 @@ void choosemax(int board1[8][8], int board2[8][8])
             }
         }
     }
-    printf("\n%d %d\n", x, y);
+    printf("%d %d", x, y);
+    // fprintf(cfptr, "\n%d %d\n", x, y);
 }
